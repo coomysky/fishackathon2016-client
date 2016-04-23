09a5e50c683eadaba6e01b131ddb2e7d2fcdfe40
ed <ultralabed@gmail.com>
2016-04-24 06:59:41 +0800

    Intergrate search get with server.

--- src/app/main/main.contorller.js
+++ src/app/main/main.contorller.js
@@ -7,14 +7,22 @@
 
     // MainController.$inject = ['config', 'logger', '$scope', '$timeout'];
     /* @ngInject */
-    function MainController($state,$scope,$rootScope,store) {
+    function MainController($state,$scope,$rootScope,store, $http) {
           var vm = this;
           vm.user = store.get('user');;
           vm.search = search;
           vm.logout = logout;
 
           function search(){
-            $state.go('main.search',{mmsi:vm.mmsi});
+            // Simple GET request example:
+            $http({
+              method: 'GET',
+              url: '/vessel/mmsi/'+ vm.mmsi
+            }).then(function successCallback(response) {
+              $state.go('main.search',{mmsi:vm.mmsi});
+            }, function errorCallback(response) {
+              console.log(response);
+            });
           }
 
           function logout(){
